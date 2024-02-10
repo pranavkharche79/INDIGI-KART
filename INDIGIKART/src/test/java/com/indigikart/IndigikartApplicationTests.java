74c60f1d879f402fbd00dabf1cde3b53e93c09d6
57369_Arjun <arjunjagtap24699@gmail.com>
2024-02-10 13:11:22 +0530

    adding skeleton of project

---
+++ INDIGIKART/src/test/java/com/indigikart/IndigikartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indigikart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndigikartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
